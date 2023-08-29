a386c8063c17f371bb65fc32dbb4c74cb3faa0a5
BuildTools <unconfigured@null.spigotmc.org>
2023-08-29 21:26:38 +0300

    Added string calculation

---
+++ src/main/java/io/github/leva25se/foglock/client/value/SimpleCalculation.java
@@ -0,0 +1,62 @@
+package io.github.leva25se.foglock.client.value;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.Camera;
+import net.minecraft.entity.Entity;
+
+public class SimpleCalculation implements StringValue {
+    @Override
+    public float getValue(String str, Camera camera, float vieDistance, boolean thickFog) {
+        Entity entity = camera.getFocusedEntity();
+        if (entity instanceof ClientPlayerEntity) {
+            str = str.replace("{underwaterVisibility}", String.valueOf(((ClientPlayerEntity)entity).getUnderwaterVisibility()));
+
+        }
+        str = str.replace("{vieDistance}", String.valueOf(vieDistance))
+                .replace("{thickFog}", String.valueOf((thickFog) ? 1 : 0));
+        char[] chars = str.toCharArray();
+        float result = 0f;
+        boolean first = true;
+        StringBuilder stringBuilder1 = new StringBuilder();
+        for (int i  = 0; i < chars.length; i++){
+            char c1 = chars[i];
+            if (Character.isDigit(c1) || c1 == '.') {
+                if (first){
+                    stringBuilder1.append(c1);
+                }
+            } else {
+                if (first){
+                    first = false;
+                    result = Float.parseFloat(stringBuilder1.toString());
+                }
+                i++;
+                float f = getNext(i, chars);
+                switch (c1){
+                    case '+' -> result += f;
+                    case '-' -> result -= f;
+                    case '*' -> result *= f;
+                    case '/' -> result /= f;
+                    case 'm' -> result = Math.max(result, f);
+                    case 'n' -> result = Math.min(result, f);
+                }
+            }
+        }
+        if (first){
+            result = Float.parseFloat(stringBuilder1.toString());
+        }
+        return result;
+    }
+
+    public  float getNext(int i, char[] chars) {
+        StringBuilder stringBuilder1 = new StringBuilder();
+        for (; i < chars.length; i += 1) {
+            char c1 = chars[i];
+            if (Character.isDigit(c1) || c1 == '.') {
+                stringBuilder1.append(c1);
+            } else {
+                return Float.parseFloat(stringBuilder1.toString());
+            }
+        }
+        return Float.parseFloat(stringBuilder1.toString());
+    }
+}
