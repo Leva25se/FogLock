c51095b95de2d789a30f7992a86298ad89274f48
BuildTools <unconfigured@null.spigotmc.org>
2024-02-28 15:23:19 +0300

    Add advanced math module

--- src/main/java/io/github/leva25se/foglock/client/value/ApplyPlaceholders.java
+++ src/main/java/io/github/leva25se/foglock/client/value/ApplyPlaceholders.java
@@ -1,18 +1,32 @@
 package io.github.leva25se.foglock.client.value;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.Camera;
 import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ApplyPlaceholders {
-
-    public String applyPlaceholders(String str, Camera camera, float vieDistance, boolean thickFog){
+    private final Pattern pattern = Pattern.compile("\\{(.*?)}");
+    public String applyPlaceholders(String str, Camera camera, float vieDistance, boolean thickFog) {
+        Matcher matcher = pattern.matcher(str);
+        StringBuilder stringBuilder = new StringBuilder(str);
         Entity entity = camera.getFocusedEntity();
-        if (entity instanceof ClientPlayerEntity) {
-            str = str.replace("{underwaterVisibility}", String.valueOf(((ClientPlayerEntity)entity).getUnderwaterVisibility()));
+        World world = MinecraftClient.getInstance().world;
+        if (world == null){
+            return str;
         }
-        str = str.replace("{vieDistance}", String.valueOf(vieDistance))
-                .replace("{thickFog}", String.valueOf((thickFog) ? 1 : 0));
-        return str;
+        while (matcher.find()) {
+            String string = str.substring(matcher.start(), matcher.end());
+            switch (string) {
+                case "{vieDistance}" -> stringBuilder.replace(matcher.start(), matcher.end(), String.valueOf(vieDistance));
+                case "{thickFog}" -> stringBuilder.replace(matcher.start(), matcher.end(), String.valueOf(thickFog ? 1 : 0));
+                case "{underwaterVisibility}" -> stringBuilder.replace(matcher.start(), matcher.end(), (entity instanceof ClientPlayerEntity) ? String.valueOf(((ClientPlayerEntity) entity).getUnderwaterVisibility()) : "0");
+            }
+        }
+        return stringBuilder.toString();
     }
 }
